7e313de64c5759357a0c19b1549c96e853f54e0f
Dave Barnwell <dave@freshsauce.co.uk>
2015-06-28 10:40:53 +0100

    Add License and adjust doc

--- model.php
+++ model.php
@@ -1,56 +1,18 @@
 <?php
-/**
-*  A simple database abstraction layer for PHP 5.3+ with very minor configuration required
-*
-*  * database table columns are auto detected and made available as public members of the class
-*  * provides CRUD, dynamic counters/finders on a database table
-*  * uses PDO for data access and exposes PDO if required
-*  * class members used to do the magic are preceeded with an underscore, be careful of column names starting with _ in your database!
-*  * requires php >=5.3 as uses "Late Static Binding" and namespaces
-*
-*  BSD Licensed.
-*
-*  Copyright (c) 2012, Dave Barnwell, www.freshsauce.co.uk, github.com/freshsauce/Model
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are met:
-*
-*  * Redistributions of source code must retain the above copyright notice, this
-*    list of conditions and the following disclaimer.
-*
-*  * Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*    In your bootstrap
-*    db\Model::connectDb('mysql:dbname=testdbname;host=127.0.0.1','testuser','testpassword');
-*
-*    Extend the model and your set to go
-*    class Category extends db\Model {
-*      static protected $_tableName = 'categories';   // database table name
-*      //static protected $_primary_column_name = 'id'; // define this if your primary key column name is not id
-*
-*      // add other methods appropriate for your class
-*
-*    }
-*    $category = Category::getById(1);
-*/
 namespace db;
 
 /**
  * Model ORM 
+ *
+ *
+ *  A simple database abstraction layer for PHP 5.3+ with very minor configuration required
+ *
+ * database table columns are auto detected and made available as public members of the class
+ * provides CRUD, dynamic counters/finders on a database table
+ * uses PDO for data access and exposes PDO if required
+ * class members used to do the magic are preceeded with an underscore, be careful of column names starting with _ in your database!
+ * requires php >=5.3 as uses "Late Static Binding" and namespaces
+ *
  *
  * @property string $created_at optional datatime in table that will automatically get updated on insert
  * @property string $updated_at optional datatime in table that will automatically get updated on insert/update
@@ -371,8 +333,7 @@ class Model
   }
 
   /**
-   * run a SELECT * FROM WHERE ...
-   * returns an array of objects of the sub-class
+   * returns an array of objects of the sub-class which match the conditions
    *
    * @param string $SQLfragment conditions, sorting, grouping and limit to apply (to right of WHERE keywords)
    * @param array $params optional params to be escaped and injected into the SQL query (standrd PDO syntax)
@@ -390,8 +351,7 @@ class Model
   }
   
   /**
-   * run a SELECT * FROM WHERE ... LIMIT 1
-   * returns an object of the sub-class
+   * returns an object of the sub-class which matches the conditions
    *
    * @param string $SQLfragment conditions, sorting, grouping and limit to apply (to right of WHERE keywords)
    * @param array $params optional params to be escaped and injected into the SQL query (standrd PDO syntax)
